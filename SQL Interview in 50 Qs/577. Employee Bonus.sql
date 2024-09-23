6361c8008f19adbad0d368ae67ea07aa82bcdedf
Shivangi Gupta <165264930+Shivi2599@users.noreply.github.com>
2024-09-23 22:02:34 +0530

    Create 577. Employee Bonus.sql

---
+++ SQL Interview in 50 Qs/577. Employee Bonus.sql
@@ -0,0 +1,34 @@
+"""
+Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+Table: Bonus
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+ 
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+Return the result table in any order.
+"""
+
+# Write your MySQL query statement below
+SELECT E.name, B.bonus
+FROM Employee as E
+LEFT JOIN Bonus as B ON E.empId = B.empId
+WHERE B.bonus < 1000 OR B.bonus IS NULL;

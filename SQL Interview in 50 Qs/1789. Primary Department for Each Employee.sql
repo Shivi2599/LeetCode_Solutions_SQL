8f793203a5071267291c3ebd04db5222667e9f0a
Shivangi Gupta <165264930+Shivi2599@users.noreply.github.com>
2025-01-18 16:35:00 +0530

    Primary Department for Each Employee

---
+++ SQL Interview in 50 Qs/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+  
+SELECT employee_id, department_id 
+FROM Employee
+WHERE primary_flag = 'Y' 
+OR 
+employee_id IN
+(
+SELECT employee_id
+FROM Employee
+GROUP by employee_id
+HAVING COUNT(*) = 1
+);

103b2b3d02d0455e4c6e34560cad75e3f3d9f667
Shivangi Gupta <165264930+Shivi2599@users.noreply.github.com>
2025-01-20 16:41:14 +0530

    Average Selling Price

--- SQL Interview in 50 Qs/1251. Average Selling Price.sql
+++ SQL Interview in 50 Qs/1251. Average Selling Price.sql
@@ -3,7 +3,7 @@
 SELECT p.product_id,
 IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) as average_price
 FROM Prices p
-JOIN UnitsSold u
+LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id
 AND u.purchase_date BETWEEN start_date AND end_date
 GROUP BY p.product_id;

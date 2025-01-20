5bf1b9ff1613cd28290473da5521fccb784389ef
Shivangi Gupta <165264930+Shivi2599@users.noreply.github.com>
2025-01-20 16:48:20 +0530

    Project Employees I

---
+++ SQL Interview in 50 Qs/1075. Project Employees I.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT p.project_id, ROUND(SUM(experience_years)/COUNT(project_id),2) as average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;

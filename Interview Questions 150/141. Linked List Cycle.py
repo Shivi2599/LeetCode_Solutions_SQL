4162e90864b9b6e462944665e6a7c507ba3dc03c
Shivangi Gupta <165264930+Shivi2599@users.noreply.github.com>
2025-06-11 12:25:27 +0530

    Create 141. Linked List Cycle.py

---
+++ Interview Questions 150/141. Linked List Cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next        ## 1 step
+            fast = fast.next.next   ## 2 steps
+
+            if slow == fast:
+                return True
+
+        return False
